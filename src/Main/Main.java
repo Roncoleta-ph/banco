b6ec6bc47cba3047cbbe32a88143e5528b5a37fb
Paulo <pauloronco2020@gmail.com>
2025-05-23 13:48:33 -0300

    espero que de certo

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -15,31 +15,8 @@ public class Main {
 
         // Dados do banco
         String url = "jdbc:postgresql://localhost:5432/Progii?ssl=false";
-        String user = "Paulo2002";
+        String user = "postgres";
         String password = "Paulo2002";
 
-        try {
-            // Conecta ao banco
-            Connection conn = DriverManager.getConnection(url, user, password);
-
-            // Prepara o SQL
-            String sql = "INSERT INTO product (name, sku, price) VALUES (?, ?, ?)";
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setString(1, name);
-            stmt.setString(2, sku);
-            stmt.setDouble(3, price);
-
-            // Executa
-            int linhas = stmt.executeUpdate();
-            if (linhas > 0) {
-                System.out.println("Produto inserido com sucesso!");
-            }
-
-            // Fecha a conexão
-            conn.close();
-
-        } catch (SQLException e) {
-            System.out.println("Erro: " + e.getMessage());
-        }
-    }
+       }
 }
